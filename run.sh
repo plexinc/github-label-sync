4ade54ce8197621635b9e81405799b264d74236a
Andy Cook <andy@cookandy.com>
2023-03-01 16:10:05 -0700

    feat: add support for configurable runtime variables

--- run.sh
+++ run.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
+[ "$ALLOW_EXISTING_LABELS" = "true" ] || [ -z "$ALLOW_EXISTING_LABELS" ] && EXISTING_LABELS_OPTION="--allow-added-labels"
+[ "$DRY_RUN" = "true" ] && DRY_RUN_OPTION="--dry-run"
+
 # loop through each of the repos and run the label sync command
 while read -r repo; do
-  github-label-sync --access-token "$GITHUB_ACCESS_TOKEN" --labels /data/labels.yaml --allow-added-labels "$repo"
+  github-label-sync --access-token "$GITHUB_ACCESS_TOKEN" --labels /data/labels.yaml "$EXISTING_LABELS_OPTION" "$DRY_RUN_OPTION" "$repo"
 done < "/data/repos.txt"
