1a8e22550822bbfff416512fb399864f005a439c
Andy Cook <andy@cookandy.com>
2023-03-02 16:34:21 -0700

    feat: add support for configurable runtime variables (part2)
    The command doesn't like empty spaces, so we have to build the command to run.

--- run.sh
+++ run.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-[ "$ALLOW_EXISTING_LABELS" = "true" ] || [ -z "$ALLOW_EXISTING_LABELS" ] && EXISTING_LABELS_OPTION="--allow-added-labels"
-[ "$DRY_RUN" = "true" ] && DRY_RUN_OPTION="--dry-run"
+command="github-label-sync --access-token $GITHUB_ACCESS_TOKEN --labels /data/labels.yaml"
+[ "$ALLOW_EXISTING_LABELS" = "true" ] || [ -z "$ALLOW_EXISTING_LABELS" ] && command="$command --allow-added-labels"
+[ "$DRY_RUN" = "true" ] && command="$command --dry-run"
 
 # loop through each of the repos and run the label sync command
 while read -r repo; do
-  github-label-sync --access-token "$GITHUB_ACCESS_TOKEN" --labels /data/labels.yaml "$EXISTING_LABELS_OPTION" "$DRY_RUN_OPTION" "$repo"
+  eval "$command" "$repo"
 done < "/data/repos.txt"
